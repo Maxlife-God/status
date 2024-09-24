f5e7904d71bcb558a113dfa44cb70f7f7ee40a2c
Maxlife-God <maxlifegod@gmail.com>
2024-09-25 03:41:54 +0800

    initial repo

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '站点监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1869221-7c0194721df759c04c5aaa9b',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://fufu.ink/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/Maxlife-God'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.fufu.ink/'
+    },
+  ],
+};
